043d63b35fc042432ff6f708bb0f33d1f80de778
Trixksq <mkolotovskij@gmail.com>
2026-03-18 11:52:45 +0200

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,31 @@
+﻿abstract class Animal
+{
+    public virtual void Speak()
+    {
+        Console.WriteLine("Тварина видає звук");
+    }
+
+    public abstract void Move();
+}
+
+class Cat : Animal
+{
+    public override void Speak()
+    {
+        Console.WriteLine("Кішка мяукає");
+    }
+
+    public override void Move()
+    {
+        Console.WriteLine("Кішка біжить");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Animal myCat = new Cat();
+        myCat.Speak();
+        myCat.Move();
+    }
+}
